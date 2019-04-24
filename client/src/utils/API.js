8cec52b5ac72b98420f3fa50ce1826f36a195d9f
Alan Wagner <alanwagner101@gmail.com>
2019-04-24 16:27:28 -0500

    setting up for heroku api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,39 +2,39 @@ import axios from "axios";
 
 export default {
     getAdmin: function() {
-        return axios.get("http://localhost:3001/api/admin")
+        return axios.get("https://morning-stream-59911.herokuapp.com/api/admin")
     },
     saveAdmin: function(adminData) {
-        return axios.post("http://localhost:3001/api/admin", adminData)
+        return axios.post("https://morning-stream-59911.herokuapp.com/api/admin", adminData)
     },
     deleteAdmin: function(id) {
-        return axios.delete("http://localhost:3001/api/admin/" + id)
+        return axios.delete("https://morning-stream-59911.herokuapp.com/api/admin/" + id)
     },
     getEvent: function() {
-        return axios.get("http://localhost:3001/api/schedule")
+        return axios.get("https://morning-stream-59911.herokuapp.com/api/schedule")
     },
     saveEvent: function(eventData) {
-        return axios.post("http://localhost:3001/api/schedule", eventData)
+        return axios.post("https://morning-stream-59911.herokuapp.com/api/schedule", eventData)
     },
     deleteEvent: function(id) {
-        return axios.delete("http://localhost:3001/api/schedule/" + id)
+        return axios.delete("https://morning-stream-59911.herokuapp.com/api/schedule/" + id)
     },
     getProdType: function() {
         return axios.get("http://localhosts:3001/api/prodtype")
     },
     saveProdType: function(prodTypeData) {
-        return axios.post("http://localhost:3001/api/prodtype", prodTypeData)
+        return axios.post("https://morning-stream-59911.herokuapp.com/api/prodtype", prodTypeData)
     },
     deleteProdType: function(id) {
-        return axios.delete("http://locaLhost:3001/api/prodtype/" + id)
+        return axios.delete("https://morning-stream-59911.herokuapp.com/api/prodtype/" + id)
     },
     getProduct: function() {
-        return axios.get("http://localhost:3001/api/products")
+        return axios.get("https://morning-stream-59911.herokuapp.com/api/products")
     },
     saveProduct: function(productData) {
-        return axios.post("http://localhost:3001/api/products", productData)
+        return axios.post("https://morning-stream-59911.herokuapp.com/api/products", productData)
     },
     deleteProduct: function(id) {
-        return axios.delete("http://localhost:3001/api/products/" + id)
+        return axios.delete("https://morning-stream-59911.herokuapp.com/api/products/" + id)
     }
 }
