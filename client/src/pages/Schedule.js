8768861762e35c90007b77ad8c768be38692f93e
Alan Wagner <alanwagner101@gmail.com>
2019-04-24 14:57:32 -0500

    just got some basic button functionality in the shop area, still have to make it look pretty

--- client/src/pages/Schedule.js
+++ client/src/pages/Schedule.js
@@ -36,6 +36,39 @@ class Schedule extends Component {
                 <Row>
                     <CalendarComp></CalendarComp>
                 </Row>
+                <Row>
+                    <Col size={"md-12"}>
+                        <Card id="storeFrontWords">
+                            <CardBody>
+                                <Container fluid>
+                                    <Row>
+                                        <Col size={"md-3"}>
+                                        </Col>
+                                        <Col size={"md-2"}>
+                                            <h2 className="infoWords">Location</h2>
+                                            <h5 className="infoWords">120 N Main St, Blue Earth, MN 56013</h5>
+                                        </Col>
+                                        <Col size={"md-2"}>
+                                            <h2 className="infoWords">Phone Number</h2>
+                                            <h5 className="infoWords">507-526-3295</h5>
+                                        </Col>
+                                        <Col size={"md-2"}>
+                                            <h2 className="infoWords">Email</h2>
+                                            <h5 className="infoWords">michelesewvac@bevcomm.net</h5>
+                                        </Col>
+                                        <Col size={"md-3"}>
+                                        </Col>
+                                    </Row>
+                                </Container>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col size={"md-12"}>
+                        <img id="divider2" src="./images/divider2.jpg" alt="./images/divider1.png" />
+                    </Col>
+                </Row>
             </Container>
         )
     }
--- client/src/pages/shop.js
+++ client/src/pages/shop.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 // import DeleteBtn from "../components/DeleteBtn";
 // import Jumbotron from "../components/Jumbotron";
-// import API from "../utils/API";
+import API from "../utils/API";
 // import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 // import { List, ListItem } from "../components/List";
@@ -10,9 +10,82 @@ import { Card, CardBody } from "../components/Card";
 
 class Shop extends Component {
 
+    state = {
+        Constant: [],
+        displayItems: [],
+        prodType: [],
+        displayTitle: "All Products"
+    }
+
+    updateProducts = () => {
+        API.getProduct().then(res => {
+            let result = [];
+            result = res.data;
+            let temp = [];
+            let endResult = [];
+            for (let i = 0; i < result.length; i++) {
+                let isSame;
+                isSame = false
+                for (let j = 0; j < temp.length; j++) {
+                    if (result[i].prodType === temp[j]) {
+                        isSame = true
+                    }
+                }
+                if (isSame === false) {
+                    temp.push(result[i].prodType);
+                }
+            }
+            for (let k = 0; k < temp.length; k++) {
+                temp[k] = temp[k].toUpperCase()
+            }
+            temp.sort();
+            for (let l = 0; l < temp.length; l++) {
+                for (let m = 0; m < result.length; m++) {
+                    if (result[m].prodType.toUpperCase() === temp[l]) {
+                        endResult.push(result[m]);
+                    }
+                }
+            }
+            this.setState({ Constant: endResult });
+            this.setState({ displayItems: endResult });
+            this.setState({ prodType: temp });
+            console.log(this.state.displayItems);
+            console.log(this.state.prodType);
+        }).catch(err => console.log(err))
+    }
+
+    componentDidMount = () => {
+        this.updateProducts()
+    }
+
+    prodTypeButton = event => {
+        event.preventDefault();
+        console.log(event.target.id);
+
+        this.setState({displayTitle: event.target.id});
+
+        let newDisplay = [];
+        for (let i = 0; i < this.state.Constant.length; i++) {
+            if (this.state.Constant[i].prodType.toUpperCase() === event.target.id) {
+                newDisplay.push(this.state.Constant[i])
+            }
+        }
+        console.log(newDisplay);
+        if(newDisplay.length) {
+            this.setState({displayItems: newDisplay})
+        } else{
+            this.setState({ displayItems: this.state.Constant })
+        }
+    }
+
     render() {
         return (
             <Container fluid>
+                <Row>
+                    <Col size={"md-12"}>
+                        <h1 id="scheduleTitle">Featured Products</h1>
+                    </Col>
+                </Row>
                 <Row>
                     <Col size={"md-1"}>
 
@@ -20,64 +93,89 @@ class Shop extends Component {
                     <Col size={"md-3"}>
                         <Card>
                             <CardBody>
-                                <FormBtn>Dummy Button</FormBtn>
-                                <FormBtn>Dummy Button</FormBtn>
-                                <FormBtn>Dummy Button</FormBtn>
-                                <FormBtn>Dummy Button</FormBtn>
+                                <h3 id="sideBarTypes">Product Types</h3>
+                                {this.state.prodType.length ? (
+                                    <div>
+                                        {this.state.prodType.map(prodType => (
+                                            <button className="prodTypeButtons" onClick={this.prodTypeButton} id={prodType}>{prodType}</button>
+                                        ))}
+                                        <button className="prodTypeButtons" onClick={this.prodTypeButton} id={"All Products"}>Display All</button>
+                                    </div>
+                                ) : (
+                                        <div>
+                                            <h4>No Product Types To Display</h4>
+                                        </div>
+                                    )}
                             </CardBody>
                         </Card>
                     </Col>
                     <Col size={"md-7"}>
                         <Card>
                             <CardBody>
-                                <h1>Title</h1>
                                 <Container fluid>
                                     <Row>
-                                        <Col size={"md-4"}>
-                                            <Card>
-                                                <h1>Words Words Words</h1>
-                                                <p>Lorem Epsum Lorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem Epsum</p>
-                                            </Card>
-                                        </Col>
-                                        <Col size={"md-4"}>
-                                            <Card>
-                                                <h1>Words Words Words</h1>
-                                                <p>Lorem Epsum Lorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem Epsum</p>
-                                            </Card>
-                                        </Col>
-                                        <Col size={"md-4"}>
-                                            <Card>
-                                                <h1>Words Words Words</h1>
-                                                <p>Lorem Epsum Lorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem Epsum</p>
-                                            </Card>
+                                        <Col size={"md-12"}>
+                                            <h2 className="productDisplayTitle">{this.state.displayTitle}</h2>
                                         </Col>
                                     </Row>
-                                    <Row>
-                                        <Col size={"md-4"}>
-                                            <Card>
-                                                <h1>Words Words Words</h1>
-                                                <p>Lorem Epsum Lorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem Epsum</p>
-                                            </Card>
-                                        </Col>
-                                        <Col size={"md-4"}>
-                                            <Card>
-                                                <h1>Words Words Words</h1>
-                                                <p>Lorem Epsum Lorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem Epsum</p>
-                                            </Card>
-                                        </Col>
-                                        <Col size={"md-4"}>
-                                            <Card>
-                                                <h1>Words Words Words</h1>
-                                                <p>Lorem Epsum Lorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem EpsumLorem Epsum</p>
-                                            </Card>
-                                        </Col>
-                                    </Row>
+                                    {this.state.displayItems.length ? (
+                                        <Row>
+                                            {this.state.displayItems.map(item => (
+                                                <Col size={"md-4"}>
+                                                    <Card>
+                                                        <CardBody>
+                                                            <h5>{item.title}</h5>
+                                                        </CardBody>
+                                                    </Card>
+                                                </Col>
+                                            ))}
+                                        </Row>
+                                    ) : (
+                                            <Row>
+                                                <Col size={"md-12"}>
+                                                    <h2 className="productDisplayTitle">No Items To Be Displayed</h2>
+                                                </Col>
+                                            </Row>
+                                        )}
                                 </Container>
                             </CardBody>
                         </Card>
                     </Col>
                     <Col size={"md-1"}>
 
+                    </Col>
+                </Row>
+                <Row>
+                    <Col size={"md-12"}>
+                        <Card id="storeFrontWords">
+                            <CardBody>
+                                <Container fluid>
+                                    <Row>
+                                        <Col size={"md-3"}>
+                                        </Col>
+                                        <Col size={"md-2"}>
+                                            <h2 className="infoWords">Location</h2>
+                                            <h5 className="infoWords">120 N Main St, Blue Earth, MN 56013</h5>
+                                        </Col>
+                                        <Col size={"md-2"}>
+                                            <h2 className="infoWords">Phone Number</h2>
+                                            <h5 className="infoWords">507-526-3295</h5>
+                                        </Col>
+                                        <Col size={"md-2"}>
+                                            <h2 className="infoWords">Email</h2>
+                                            <h5 className="infoWords">michelesewvac@bevcomm.net</h5>
+                                        </Col>
+                                        <Col size={"md-3"}>
+                                        </Col>
+                                    </Row>
+                                </Container>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col size={"md-12"}>
+                        <img id="divider2" src="./images/divider2.jpg" alt="./images/divider1.png" />
                     </Col>
                 </Row>
             </Container>
